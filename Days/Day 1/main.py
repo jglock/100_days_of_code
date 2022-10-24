23a8ba615d288cd76e60331730e7b193d006516c
JulGlo <juliusglock@gmail.com>
2022-10-24 16:37:44 +0200

    Days 1 & 2

---
+++ Days/Day 1/main.py
@@ -0,0 +1,11 @@
+"""
+Simple Band Name Generator, that asks the user via input and prints out the combination
+"""
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city} {pet}")
---
+++ Days/Day 2/main.py
@@ -0,0 +1,13 @@
+"""
+Simple Tip Calculator which calculates how much one must pay taking the number of people, the total bill and the percentage tip by input
+"""
+
+print("Welcome to the tip calculator.")
+
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+total = bill * (1 + tip / 100)
+
+print(f"Each person should pay: ${total / people:.2f}")
