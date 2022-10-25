7f5e7e9a378e396ae44fbfa81a41e9cd5bb48ce9
GSG\Glock <glock@gsg-online.com>
2022-10-25 18:55:48 +0200

    Day 6

---
+++ Days/Day 6 - Reeborgs World - Escape the Maze/main.py
@@ -0,0 +1,18 @@
+"""
+This Code is the solution for the challenge on this website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+You have to use if statements as well as create and call some simple functions to escape from the maze.
+"""
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
