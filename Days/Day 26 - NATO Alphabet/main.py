872d0736542d41cf75574f049503cbacaa0672ce
GSG\Glock <juliusglock@gmail.com>
2023-02-13 18:59:56 +0100

    Day 26

---
+++ Days/Day 26 - NATO Alphabet/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+#print(alphabet)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+while True:
+    name = input("Enter a word: ").upper()
+
+    print([alphabet[alphabet['letter']== character]['code'].values[0] for character in name])
+
+    if name == "END":
+        break
